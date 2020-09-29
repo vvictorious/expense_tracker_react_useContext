01d15616c334a4b275349628dbe641e0b52c0b88
vvictorious <vvictorious30@gmail.com>
2020-09-29 13:53:00 -0600

    had to use the Math.abs method to get rid of that weird double negative

--- expense-tracker/src/components/Transaction.js
+++ expense-tracker/src/components/Transaction.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 export const Transaction = ({transaction}) => {
+
+    const sign = transaction.amount < 0 ? '-' : '+'
+
     return (
         <li>
-            {transaction.text}
+            {transaction.text} <span>{sign}{Math.abs(transaction.amount)}</span><button >x</button>
         </li>
     )
 }
